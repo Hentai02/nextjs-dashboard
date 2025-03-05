9b80b6d94f43fbc85f352ac90c39ebc0910cbe3d
Hentai02 <hentaikis@gmail.com>
2025-03-05 23:50:53 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, ZCOOL_XiaoWei } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const zcool_xiaowei = ZCOOL_XiaoWei({
+  subsets: ["latin"],
+  weight: "400",
+});
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
