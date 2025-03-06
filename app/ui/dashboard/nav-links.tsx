96431bc59baae67a01a85eb7a051a4871d3af611
Hentai02 <hentaikis@gmail.com>
2025-03-06 17:38:14 +0800

    adding search and pagination

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -23,7 +24,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log(pathname);
   return (
     <>
       {links.map((link) => {
